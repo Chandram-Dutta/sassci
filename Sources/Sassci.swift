b678716ab6e536c6ee169c746ad23fe1f76d8f8e
Chandram-Dutta <chandramdutta2004@gmail.com>
2024-05-05 21:33:49 +0530

    build: Add Rainbow package dependency
    Added Rainbow package as a dependency in the Package.swift file to enable
    color functionality in the ASCII art generation process. Also added a Rainbow
    import statement in the Sassci.swift file for color processing.

--- Sources/Sassci.swift
+++ Sources/Sassci.swift
@@ -1,36 +1,45 @@
 import ArgumentParser
 import SwiftImage
+import Rainbow
 
 @main
 struct Sassci: ParsableCommand {
-	
+
 	static let configuration = CommandConfiguration(
 		commandName: "sassci",
 		abstract: "A CLI utility to create ASCII art from images",
 		version: "1.0.0"
 	)
-	
+
 	@Argument(help: "The path to the image file to convert to ASCII art.")
 	var imagePath: String
-	
+
 	@Option(help: "Scale to resize the image")
 	var scale = 1.0
-	
+
 	@Option(help: "Each ASCII character is multiplied to make the image look less squashed")
 	var charMultiplier = 3
-	
+
 	@Flag(help: "Inverts the brightness of the image")
 	var invert = false
 	
+	@Flag(help: "Adds color to the picture")
+	var color = true
+
 	func run() throws {
 		var image = Image<RGBA<UInt8>>(contentsOfFile: imagePath)!
 		image = image.resizedTo(width: Int(Double(image.width) * scale), height: Int(Double(image.height) * scale))
 		print("\(image.width), \(image.height)")
 		var pixelCount = 0
 		for pixel in image {
-			print(pixel.toBrightness().toASCII(charMultiplier: charMultiplier, invert: invert), terminator: "")
-			pixelCount += 1
+			if color {
+				print(pixel.toBrightness().toASCII(charMultiplier: charMultiplier, invert: invert).bit24(pixel.red, pixel.green, pixel.blue), terminator: "")
+			} else {
+				print(pixel.toBrightness().toASCII(charMultiplier: charMultiplier, invert: invert), terminator: "")
+			}
 			
+			pixelCount += 1
+
 			if pixelCount % Int(image.width) == 0 {
 				print()
 			}
@@ -39,7 +48,7 @@ struct Sassci: ParsableCommand {
 }
 
 extension RGBA<UInt8> {
-	func toBrightness() -> Int{
+	func toBrightness() -> Int {
 		return ((Int(self.red) + Int(self.green) + Int(self.blue)) / 3)
 	}
 }
